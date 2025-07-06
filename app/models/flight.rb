9aa864d4a34ed816d3ce2d5492b88440cf0d4725
megpeer <148973963+megpeer@users.noreply.github.com>
2025-07-05 21:19:04 -0400

    database migration for booking, and associations added

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,7 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport', foreign_key: 'arrival_airport_id'
   belongs_to :departing_airport, class_name: 'Airport', foreign_key: 'departing_airport_id'
+  has_many :bookings
 
   def event_date_formatted
     flight.date.strftime('%m/%d/%y')
