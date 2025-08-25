3305253d5be810b591d5c077cbcb7a20677b3dd4
megpeer <148973963+megpeer@users.noreply.github.com>
2025-08-25 19:03:59 -0400

    adding mailers

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  default from: 'confirm@flights.com'
+  def confirmation_email
+    @passenger = params[:name]
+    @url = 'http://localhost:3000'
+    mail(to: @user.email, subject: 'thanks for booking!')
+  end
 end
