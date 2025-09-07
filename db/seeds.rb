7db4ffa8ee117b747fbf138e8e4f937f26a69d12
megpeer <148973963+megpeer@users.noreply.github.com>
2025-09-07 14:48:30 -0400

    fixed DB seeing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,48 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# DESTROY OLD ENTRIES
 Flight.destroy_all
 Airport.destroy_all
+Booking.destroy_all
+Passenger.destroy_all
 
-yyz = Airport.create!(code: 'YYZ', city: 'Toronto')
-yvr = Airport.create!(code: 'YVR', city: 'Vancouver')
-yyc = Airport.create!(code: 'YYC', city: 'Calgary')
-yeg = Airport.create!(code: 'YEG', city: 'Edmonton')
-yul = Airport.create!(code: 'YUL', city: 'Montreal')
-yyt = Airport.create!(code: 'YYT', city: "St. John's")
-ywg = Airport.create!(code: 'YWG', city: 'Winnipeg')
-yfc = Airport.create!(code: 'YFC', city: 'Fredericton')
-yhc = Airport.create!(code: 'YHC', city: 'Halifax')
-yow = Airport.create!(code: 'YOW', city: 'Ottawa')
-yqb = Airport.create!(code: 'YQB', city: 'Quebec City')
-yqx = Airport.create!(code: 'YQX', city: 'Gander')
-yqm = Airport.create!(code: 'YQM', city: 'Moncton')
-p "Created #{Airport.count} Airports"
-1000.times do
-  departing_airport, arrival_airport = Airport.all.sample(2).map(&:id)
-  date = Date.today + rand(10..20).days
-  duration = rand(60..500).to_i
-
-  Flight.find_or_create_by!(departing_airport_id: departing_airport,
-                            arrival_airport_id: arrival_airport,
-                            date: date,
-                            duration: duration)
+# CREATE ALL AIRPORTS
+airports = [
+  { code: 'YYZ', city: 'Toronto' },
+  { code: 'YVR', city: 'Vancouver' },
+  { code: 'YYC', city: 'Calgary' },
+  { code: 'YEG', city: 'Edmonton' },
+  { code: 'YUL', city: 'Montreal' },
+  { code: 'YYT', city: "St. John's" },
+  { code: 'YWG', city: 'Winnipeg' },
+  { code: 'YFC', city: 'Fredericton' },
+  { code: 'YHC', city: 'Halifax' },
+  { code: 'YOW', city: 'Ottawa' },
+  { code: 'YQB', city: 'Quebec City' },
+  { code: 'YQX', city: 'Gander' },
+  { code: 'YQM', city: 'Moncton' }
+]
+airports.each do |data|
+  Airport.create!(data)
 end
-p "Created #{Flight.count} Flights"
+p "Created #{Airport.count} Airports"
+
+# CREATE FLIGHTS - OPTION FOR EVERY DAY, EVERY AIRPORT
+airports = Airport.all
+start_date = Date.today
+end_date = start_date + 30.days
+
+(start_date..end_date).each do |date|
+  airports.each do |departure_airport|
+    airports.each do |arrival_airport|
+      next if departure_airport == arrival_airport
+
+      duration = rand(60..500).to_i
+      Flight.create!(
+        departing_airport: departure_airport,
+        arrival_airport: arrival_airport,
+        date: date,
+        duration: duration
+      )
+    end
+  end
+end
+p "Created #{Flight.count} flights"
